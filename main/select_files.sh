14b23050abcf824af7a61b0930c6202c6d16aa43
Shigetoshi Hirano <hirano.shigetoshi@classmethod.jp>
2019-11-24 13:08:36 +0900

    ファイル検索部分の関数化

--- main/select_files.sh
+++ main/select_files.sh
@@ -1,23 +1,33 @@
 #!/usr/bin/env bash
 set -eu
 
+function direstories() {
+  depth_option=""
+  if [[ $# > 1 ]]; then
+    depth_option="-maxdepth $2"
+  fi
+  cd $1 && find . ${depth_option} -type d 2>/dev/null \
+    | sed 's%$%/%' \
+    | sed 's%^\./%%' \
+    | grep -v '^\s*$'
+}
+
+function files() {
+  depth_option=""
+  if [[ $# > 1 ]]; then
+    depth_option="-maxdepth $2"
+  fi
+  cd $1 && pt -g ^ ${depth_option} 2>/dev/null \
+    | sed 's%^\./%%'
+}
+
 dir=$(cd $1 && pwd)
 readonly DEPTH=$(echo "${dir}" | tr '/' '\n' | wc -l)
 if [[ ${DEPTH} -gt 3 ]]; then
-  cd $dir && find . -type d 2>/dev/null \
-    | sed 's%$%/%' \
-    | sed 's%^\./%%' \
-    | grep -v '^\s*$' \
-    && true
-  cd $dir && pt -g ^ 2>/dev/null \
-    | sed 's%^\./%%'
+  direstories ${dir} && true
+  files ${dir}
 else
-  cd $dir && find . -maxdepth 3 -type d 2>/dev/null \
-    | sed 's%$%/%' \
-    | sed 's%^\./%%' \
-    | grep -v '^\s*$' \
-    && true
-  cd $dir && pt -g ^ --depth=3 2>/dev/null \
-    | sed 's%^\./%%'
+  direstories ${dir} 3 && true
+  files ${dir} 3
 fi
 
