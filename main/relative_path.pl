5775233d6c877437e334e041d3b35a6c31ab9531
Shigetoshi Hirano <hirano.shigetoshi@classmethod.jp>
2019-11-26 17:10:49 +0900

    ディレクトリ移動ができるよう大幅改良

---
+++ main/relative_path.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+my $prefix = quotemeta($ARGV[0]);
+
+while (<STDIN>) {
+    s/[\r\n]//g;
+    if ($_ eq "../") {
+        print $_ . "\n";
+        next;
+    }
+    if (/^$prefix(.*)$/) {
+        print($1 . "\n");
+    } else {
+        print $_ . "\n";
+    }
+}
+
--- main/select_files.sh
+++ main/select_files.sh
@@ -2,32 +2,22 @@
 set -eu
 
 function direstories() {
-  depth_option=""
-  if [[ $# > 1 ]]; then
-    depth_option="-maxdepth $2"
-  fi
-  cd $1 && find . ${depth_option} -type d 2>/dev/null \
+  echo '../'
+  find "$1" -type d 2>/dev/null \
+    | grep -vxF "$1" \
+    | sed 's%//\+%/%g' \
     | sed 's%$%/%' \
     | sed 's%^\./%%' \
     | grep -v '^\s*$'
 }
 
 function files() {
-  depth_option=""
-  if [[ $# > 1 ]]; then
-    depth_option="-maxdepth $2"
-  fi
-  cd $1 && pt -g ^ ${depth_option} 2>/dev/null \
-    | sed 's%^\./%%'
+  pt -g ^ "$1" 2>/dev/null \
+    | sed 's%//\+%/%g' \
+    | sed 's%^\./%%' \
+    | grep -v '^\s*$'
 }
 
-dir=$(cd $1 && pwd)
-readonly DEPTH=$(echo "${dir}" | tr '/' '\n' | wc -l)
-if [[ ${DEPTH} -gt 3 ]]; then
-  direstories ${dir} && true
-  files ${dir}
-else
-  direstories ${dir} 3 && true
-  files ${dir} 3
-fi
+direstories "$1"
+files "$1"
 
