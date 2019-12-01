26424716bea9b7df2946b4ae9ccc90d7a660c6f8
Shigetoshi Hirano <hirano.shigetoshi@classmethod.jp>
2019-12-01 23:09:22 +0900

    BrokenPipeの捨て方を修正

--- main/norm_path.py
+++ main/norm_path.py
@@ -1,8 +1,5 @@
 import os
 import sys
-import re
-from signal import signal, SIGPIPE, SIG_DFL
-signal(SIGPIPE, SIG_DFL)
 
 import argparse
 p = argparse.ArgumentParser()
@@ -12,19 +9,23 @@ p.add_argument('-t', '--tilde', action='store_true', help='$HOME to "~"')
 p.add_argument('-d', '--dir', action='store_true', help='put suffix "/"')
 args = p.parse_args()
 
-line = sys.stdin.readline()
-while line:
-    line = line.strip("\n")
-    if args.absolute:
-        line = os.path.abspath(line)
-    elif args.relative:
-        line = os.path.relpath(line)
-    else:
-        line = os.path.normpath(line)
-    if args.tilde and line.startswith(os.environ['HOME']):
-        line = line.replace(os.environ['HOME'], '~')
-    if args.dir and line != '/':
-        line += '/'
-    print(line)
+try:
     line = sys.stdin.readline()
-
+    while line:
+        line = line.strip("\n")
+        if args.absolute:
+            line = os.path.abspath(line)
+        elif args.relative:
+            line = os.path.relpath(line)
+        else:
+            line = os.path.normpath(line)
+        if args.tilde and line.startswith(os.environ['HOME']):
+            line = line.replace(os.environ['HOME'], '~')
+        if args.dir and line != '/':
+            line += '/'
+        print(line)
+        line = sys.stdin.readline()
+except BrokenPipeError:
+    devnull = os.open(os.devnull, os.O_WRONLY)
+    os.dup2(devnull, sys.stdout.fileno())
+    sys.exit(1)
