274ff9a0f335d53635f6e933caee38e3b8cdb9de
Shigetoshi Hirano <hirano.shigetoshi@classmethod.jp>
2020-12-06 13:26:03 +0900

    /終わりの場合はそのディレクトリで検索

--- select-files.zsh
+++ select-files.zsh
@@ -5,7 +5,13 @@ function select_files() {
   left=$(sed -n '1p' <<< "${strings}")
   center=$(sed -n '2p' <<< "${strings}")
   right=$(sed -n '3p' <<< "${strings}")
-  out=$(fzfyml3 run ${SELECT_FILES_TOOL_DIR}/main/select_files.yml . "$center")
+  if [[ "$center" = "/" ]]; then
+    out=$(fzfyml3 run ${SELECT_FILES_TOOL_DIR}/main/select_files.yml "/" "")
+  elif echo "$center" | grep -q '/$'; then
+    out=$(fzfyml3 run ${SELECT_FILES_TOOL_DIR}/main/select_files.yml "${center%%/}" "")
+  else
+    out=$(fzfyml3 run ${SELECT_FILES_TOOL_DIR}/main/select_files.yml . "$center")
+  fi
   if [[ -n "$out" ]]; then
     BUFFER="${left}${out}${right}"
     CURSOR=$((${#BUFFER} - ${#right} + 1))
