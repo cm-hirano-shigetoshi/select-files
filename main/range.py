b46eaf59790aa3b332aabdc74f5e7e0fe588544a
Shigetoshi Hirano <hirano.shigetoshi@classmethod.jp>
2020-12-06 13:01:03 +0900

    全体的に見直し

--- main/range.py
+++ main/range.py
@@ -4,10 +4,12 @@ import sys
 import shlex
 
 quotes = shlex.shlex().quotes
-index = int(sys.argv[1])
-buf = sys.argv[2]
+buf = sys.argv[1]
+index = int(sys.argv[2])
 m = re.match(r".*[\s{}](\S*)$".format(quotes), ' ' + buf[:index])
 query = m.group(1)
-print(buf[:(index - len(m.group(1)))], end='\x00')
-print(m.group(1), end='\x00')
-print(buf[index:], end='')
+print(buf[:(index - len(m.group(1)))])
+print(m.group(1))
+if index == len(buf) or buf[index] != ' ':
+    print(' ', end='')
+print(buf[index:])
--- select-files.zsh
+++ select-files.zsh
@@ -1,40 +1,17 @@
 SELECT_FILES_TOOL_DIR=${SELECT_FILES_TOOL_DIR-${0:A:h}}
 
 function select_files() {
-  local strings left center right dir query
-  strings=$(python ${SELECT_FILES_TOOL_DIR}/main/range.py $CURSOR "$BUFFER")
-  left=$(sed -z -n '1p' <<< "${strings}" | sed 's/\x0//')
-  center=$(sed -z -n '2p' <<< "${strings}" | sed 's/\x0//')
-  right=$(sed -z -n '3p' <<< "${strings}" | sed 's/\x0//')
-  #echo "\"$left\"" | xxd
-  #echo "\"$center\"" | xxd
-  #echo "\"$right\"" | xxd
-
-  center="$(echo "${center}" | sed 's%\([^/]\)$%\1/%')"
-  dir="$(echo "${center}" | sed -e 's%^$%.%' -e "s%^~%${HOME}%")"
-  if [[ -d "${dir}" ]]; then
-    query=""
-  else
-    center="$(echo "${center}" | sed 's%[^/]\+/$%%')"
-    query="$(basename "${dir}")"
-    dir="$(dirname "${dir}")"
-  fi
-  out=$(fzfyml3 run ${SELECT_FILES_TOOL_DIR}/main/select_files.yml "${center}" "${dir}" "${query}")
+  strings=$(python ${SELECT_FILES_TOOL_DIR}/main/range.py "$BUFFER" $CURSOR)
+  left=$(sed -n '1p' <<< "${strings}")
+  center=$(sed -n '2p' <<< "${strings}")
+  right=$(sed -n '3p' <<< "${strings}")
+  out=$(fzfyml3 run ${SELECT_FILES_TOOL_DIR}/main/select_files.yml . "$center")
   if [[ -n "$out" ]]; then
     BUFFER="${left}${out}${right}"
-    CURSOR=$((${#BUFFER} - ${#right}))
+    CURSOR=$((${#BUFFER} - ${#right} + 1))
     zle redisplay
   fi
 }
 zle -N select_files
-
-function select_files_test() {
-  result=$(python $SELECT_FILES_TOOL_DIR/test.py)
-  if [[ -n $result ]]; then
-    BUFFER=$result
-    zle redisplay
-  fi
-}
-zle -N select_files_test
 bindkey '\e[Z' select_files
 
